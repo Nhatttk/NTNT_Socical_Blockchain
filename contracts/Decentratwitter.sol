20d1bcd1712ff524f5362e9fe661fd7ca23e7b36
toilaanhtai21it <tainda.21it@vku.udn.vn>
2025-05-05 21:51:37 +0700

    reactcomment

--- contracts/Decentratwitter.sol
+++ contracts/Decentratwitter.sol
@@ -1,11 +1,4 @@
-//SPDX-License-Identifier: MIT
-// Two pages to app: Home and Profile
-// Must have NFT to post content
-// NFT will represent avatar. Store username and image as NFT metadata
-// For profile page, if user has not created an NFT yet then a form will appear that allows users to mint their own NFT profile.
-// For profile page, if user has created an NFT then it will display the users profile picture and username at the top of the page
-// and below that there will be a form at the bottom that allows users to create a new NFT for their profile.
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
@@ -16,14 +9,31 @@ contract Decentratwitter is ERC721URIStorage {
     uint256 public tokenCount;
     uint256 public postCount;
     mapping(uint256 => Post) public posts;
-    // address --> nft id
     mapping(address => uint256) public profiles;
+    // Mapping for comments: postId => commentId => Comment
+    mapping(uint256 => mapping(uint256 => Comment)) public comments;
+    // Mapping for comment counts per post
+    mapping(uint256 => uint256) public commentCounts;
+    // Mapping for comment existence tracking
+    mapping(uint256 => mapping(uint256 => bool)) public commentExists;
+    // Mapping for reactions: postId => address => bool
+    mapping(uint256 => mapping(address => bool)) public postReactions;
+    // Mapping for reaction counts per post
+    mapping(uint256 => uint256) public reactionCounts;
 
     struct Post {
         uint256 id;
         string hash;
         uint256 tipAmount;
         address payable author;
+        bool exists; // Add a flag to check if post exists
+    }
+
+    struct Comment {
+        uint256 id;
+        string hash;
+        address payable author;
+        uint256 timestamp;
     }
 
     event PostCreated(
@@ -38,6 +48,51 @@ contract Decentratwitter is ERC721URIStorage {
         string hash,
         uint256 tipAmount,
         address payable author
+    );
+
+    event PostUpdated(
+        uint256 id,
+        string hash,
+        address payable author
+    );
+
+    event PostDeleted(
+        uint256 id,
+        address payable author
+    );
+
+    event CommentCreated(
+        uint256 postId,
+        uint256 commentId,
+        string hash,
+        address payable author,
+        uint256 timestamp
+    );
+
+    event CommentUpdated(
+        uint256 postId,
+        uint256 commentId,
+        string hash,
+        address payable author,
+        uint256 timestamp
+    );
+
+    event CommentDeleted(
+        uint256 postId,
+        uint256 commentId,
+        address payable author
+    );
+    
+    event ReactionAdded(
+        uint256 postId,
+        address user,
+        uint256 reactionCount
+    );
+    
+    event ReactionRemoved(
+        uint256 postId,
+        address user,
+        uint256 reactionCount
     );
 
     constructor() ERC721("Decentratwitter", "DAPP") {}
@@ -59,46 +114,213 @@ contract Decentratwitter is ERC721URIStorage {
     }
 
     function uploadPost(string memory _postHash) external {
-        // Check that the user owns an nft
         require(
             balanceOf(msg.sender) > 0,
             "Must own a decentratwitter nft to post"
         );
-        // Make sure the post hash exists
         require(bytes(_postHash).length > 0, "Cannot pass an empty hash");
-        // Increment post count
         postCount++;
-        // Add post to the contract
-        posts[postCount] = Post(postCount, _postHash, 0, payable(msg.sender));
-        // Trigger an event
+        posts[postCount] = Post(postCount, _postHash, 0, payable(msg.sender), true);
         emit PostCreated(postCount, _postHash, 0, payable(msg.sender));
     }
 
-    function tipPostOwner(uint256 _id) external payable {
-        // Make sure the id is valid
+    function editPost(uint256 _id, string memory _postHash) external {
         require(_id > 0 && _id <= postCount, "Invalid post id");
-        // Fetch the post
+        require(bytes(_postHash).length > 0, "Cannot pass an empty hash");
         Post memory _post = posts[_id];
-        require(_post.author != msg.sender, "Cannot tip your own post");
-        // Pay the author by sending them Ether
-        _post.author.transfer(msg.value);
-        // Increment the tip amount
-        _post.tipAmount += msg.value;
-        // Update the image
+        require(_post.exists, "Post does not exist");
+        require(_post.author == msg.sender, "Only post author can edit");
+        
+        _post.hash = _postHash;
         posts[_id] = _post;
-        // Trigger an event
+        emit PostUpdated(_id, _postHash, payable(msg.sender));
+    }
+
+    function deletePost(uint256 _id) external {
+        require(_id > 0 && _id <= postCount, "Invalid post id");
+        Post memory _post = posts[_id];
+        require(_post.exists, "Post does not exist");
+        require(_post.author == msg.sender, "Only post author can delete");
+        
+        _post.exists = false;
+        posts[_id] = _post;
+        emit PostDeleted(_id, payable(msg.sender));
+    }
+    
+    function toggleReaction(uint256 _id) external {
+        require(_id > 0 && _id <= postCount, "Invalid post id");
+        require(
+            balanceOf(msg.sender) > 0,
+            "Must own a decentratwitter nft to react"
+        );
+        Post memory _post = posts[_id];
+        require(_post.exists, "Post does not exist");
+        
+        // Toggle reaction state
+        if (postReactions[_id][msg.sender]) {
+            // Remove reaction
+            postReactions[_id][msg.sender] = false;
+            reactionCounts[_id]--;
+            emit ReactionRemoved(_id, msg.sender, reactionCounts[_id]);
+        } else {
+            // Add reaction
+            postReactions[_id][msg.sender] = true;
+            reactionCounts[_id]++;
+            emit ReactionAdded(_id, msg.sender, reactionCounts[_id]);
+        }
+    }
+    
+    function hasReacted(uint256 _id, address _user) public view returns (bool) {
+        require(_id > 0 && _id <= postCount, "Invalid post id");
+        Post memory _post = posts[_id];
+        require(_post.exists, "Post does not exist");
+        
+        return postReactions[_id][_user];
+    }
+
+    function tipPostOwner(uint256 _id) external payable {
+        require(_id > 0 && _id <= postCount, "Invalid post id");
+        Post memory _post = posts[_id];
+        require(_post.exists, "Post does not exist");
+        require(_post.author != msg.sender, "Cannot tip your own post");
+        _post.author.transfer(msg.value);
+        _post.tipAmount += msg.value;
+        posts[_id] = _post;
         emit PostTipped(_id, _post.hash, _post.tipAmount, _post.author);
     }
 
-    // Fetches all the posts
+    function addComment(uint256 _postId, string memory _commentHash) external {
+        require(_postId > 0 && _postId <= postCount, "Invalid post id");
+        require(
+            balanceOf(msg.sender) > 0,
+            "Must own a decentratwitter nft to comment"
+        );
+        require(bytes(_commentHash).length > 0, "Cannot pass an empty hash");
+        Post memory _post = posts[_postId];
+        require(_post.exists, "Post does not exist");
+        
+        commentCounts[_postId]++;
+        uint256 commentId = commentCounts[_postId];
+        comments[_postId][commentId] = Comment(
+            commentId,
+            _commentHash,
+            payable(msg.sender),
+            block.timestamp
+        );
+        // Mark comment as existing
+        commentExists[_postId][commentId] = true;
+        
+        emit CommentCreated(
+            _postId,
+            commentId,
+            _commentHash,
+            payable(msg.sender),
+            block.timestamp
+        );
+    }
+
+    function editComment(
+        uint256 _postId,
+        uint256 _commentId,
+        string memory _commentHash
+    ) external {
+        require(_postId > 0 && _postId <= postCount, "Invalid post id");
+        require(
+            _commentId > 0 && _commentId <= commentCounts[_postId],
+            "Invalid comment id"
+        );
+        Post memory _post = posts[_postId];
+        require(_post.exists, "Post does not exist");
+        require(commentExists[_postId][_commentId], "Comment does not exist");
+        
+        Comment memory _comment = comments[_postId][_commentId];
+        require(
+            _comment.author == msg.sender,
+            "Only the comment author can edit"
+        );
+        require(bytes(_commentHash).length > 0, "Cannot pass an empty hash");
+        _comment.hash = _commentHash;
+        _comment.timestamp = block.timestamp;
+        comments[_postId][_commentId] = _comment;
+        emit CommentUpdated(
+            _postId,
+            _commentId,
+            _commentHash,
+            payable(msg.sender),
+            block.timestamp
+        );
+    }
+
+    function deleteComment(uint256 _postId, uint256 _commentId) external {
+        require(_postId > 0 && _postId <= postCount, "Invalid post id");
+        require(
+            _commentId > 0 && _commentId <= commentCounts[_postId],
+            "Invalid comment id"
+        );
+        Post memory _post = posts[_postId];
+        require(_post.exists, "Post does not exist");
+        require(commentExists[_postId][_commentId], "Comment does not exist or already deleted");
+        
+        Comment memory _comment = comments[_postId][_commentId];
+        require(
+            _comment.author == msg.sender,
+            "Only the comment author can delete"
+        );
+        
+        // Instead of using delete, mark as not existing but keep the data
+        commentExists[_postId][_commentId] = false;
+        emit CommentDeleted(_postId, _commentId, payable(msg.sender));
+    }
+
     function getAllPosts() external view returns (Post[] memory _posts) {
-        _posts = new Post[](postCount);
-        for (uint256 i = 0; i < _posts.length; i++) {
-            _posts[i] = posts[i + 1];
+        uint256 activePosts = 0;
+        
+        // First, count active posts
+        for (uint256 i = 1; i <= postCount; i++) {
+            if (posts[i].exists) {
+                activePosts++;
+            }
+        }
+        
+        _posts = new Post[](activePosts);
+        uint256 currentIndex = 0;
+        
+        // Then add all active posts to the array
+        for (uint256 i = 1; i <= postCount; i++) {
+            if (posts[i].exists) {
+                _posts[currentIndex] = posts[i];
+                currentIndex++;
+            }
         }
     }
 
-    // Fetches all of the users nfts
+    function getComments(
+        uint256 _postId
+    ) external view returns (Comment[] memory _comments) {
+        require(_postId > 0 && _postId <= postCount, "Invalid post id");
+        Post memory _post = posts[_postId];
+        require(_post.exists, "Post does not exist");
+        
+        // Count existing comments first
+        uint256 existingCommentCount = 0;
+        for (uint256 i = 1; i <= commentCounts[_postId]; i++) {
+            if (commentExists[_postId][i]) {
+                existingCommentCount++;
+            }
+        }
+        
+        _comments = new Comment[](existingCommentCount);
+        uint256 currentIndex = 0;
+        
+        // Only include existing comments
+        for (uint256 i = 1; i <= commentCounts[_postId]; i++) {
+            if (commentExists[_postId][i]) {
+                _comments[currentIndex] = comments[_postId][i];
+                currentIndex++;
+            }
+        }
+    }
+
     function getMyNfts() external view returns (uint256[] memory _ids) {
         _ids = new uint256[](balanceOf(msg.sender));
         uint256 currentIndex;
